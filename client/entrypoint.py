496e0428483a18bc39ea41d016a543a37ab2d035
Joseph Bylund <joseph.bylund@gmail.com>
2023-03-05 11:42:44 -0500

    (joe) init

---
+++ client/entrypoint.py
@@ -0,0 +1,21 @@
+import argparse
+import logging
+import time
+
+logger = logging.getLogger("api")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    return vars(parser.parse_args())
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    get_args()
+    while True:
+        time.sleep(1)
+
+
+if "__main__" == __name__:
+    main()
