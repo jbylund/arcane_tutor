fde5dc55da4f5375822f3cbb2e1d9c9df9f0e350
Joe Bylund <joseph.bylund@gmail.com>
2025-09-06 10:54:44 -0400

    (joe) compute latency two ways and use the min

--- api/parsing/__init__.py
+++ api/parsing/__init__.py
@@ -9,7 +9,7 @@ from .nodes import (
     QueryNode,
     StringValueNode,
 )
-from .parsing_f import generate_sql_query, parse_scryfall_query, parse_search_query, balance_partial_query
+from .parsing_f import balance_partial_query, generate_sql_query, parse_scryfall_query, parse_search_query
 
 node_types = [AndNode, AttributeNode, BinaryOperatorNode, NotNode, NumericValueNode, OrNode, Query, QueryNode, StringValueNode]
 functions = [parse_search_query, generate_sql_query, parse_scryfall_query, balance_partial_query]
