8b3d5411307ab23e553e5e3c2cc6c6a5ddf87146
Joe Bylund <joseph.bylund@gmail.com>
2025-09-23 15:25:51 -0400

    (joe) ugh

--- api/utils/db_utils.py
+++ api/utils/db_utils.py
@@ -7,14 +7,17 @@ import os
 import pathlib
 import random
 import sys
+import time
 
+import docker
+import docker.errors
 import orjson
 import psycopg
 import psycopg.types.json
 import psycopg_pool
 
 logger = logging.getLogger(__name__)
-
+CONFLICT = 409
 
 def get_pg_creds() -> dict[str, str]:
     """Get postgres credentials from the environment."""
@@ -34,15 +37,38 @@ def get_testcontainers_creds() -> dict[str, str]:
         username="testuser",
         password="testpass",  # noqa: S106
         dbname="testdb",
-    ).with_bind_ports(5432, exposed_port)
-    container.start()
-    return {
+    ).with_bind_ports(5432, exposed_port).with_name("postgres-test")
+
+    connection_info = {
         "dbname": "testdb",
-        "host": container.get_container_host_ip(),
         "password": "testpass",
-        "port": container.get_exposed_port(5432),
         "user": "testuser",
     }
+    connection_info["host"] = "localhost"
+    try:
+        container.start()
+    except docker.errors.APIError as oops:
+        if oops.status_code != CONFLICT:
+            raise
+        docker_client = docker.from_env()
+        deadline = time.monotonic() + 10
+        while time.monotonic() < deadline:
+            containers = docker_client.containers.list(
+                filters={"name": "postgres-test"},
+            )
+            if containers:
+                break
+        else:
+            msg = "Ephemeral postgres container not found"
+            raise RuntimeError(msg)
+        the_container = containers[0]
+        container_attrs = the_container.attrs
+        network_settings = container_attrs["NetworkSettings"]
+        connection_info["port"] = network_settings["Ports"].popitem()[1][0]["HostPort"]
+    else:
+        connection_info["port"] = container.get_exposed_port(5432)
+    logger.info("Connection info in pid %d: %s", os.getpid(), connection_info)
+    return connection_info
 
 
 def configure_connection(conn: psycopg.Connection) -> None:
